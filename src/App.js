0c15eaf99d4eef6c4218aa3de9c187c9ced6d104
bk10d <bhavanshree.k@10decoders.in>
2024-07-20 14:01:08 +0530

    nothing

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Docker image pushed to DockerHub successfully!
+        
         </p>
        
       </header>
